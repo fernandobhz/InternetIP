dc257c9d52ca29faee6b9740df00c7c6472276a2
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 19:09:11 -0300

    old project migrated

---
+++ InternetIP/Main.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Net
+
+Public Module Main
+
+    Sub Main()
+        Console.WriteLine(GetInternetIP)
+    End Sub
+
+    Friend Function GetInternetIP() As String
+        Dim W As New WebClient
+        Dim ip As String = W.DownloadString("http://checkip.dyndns.org/")
+
+        Dim pStart As Integer = ip.IndexOf("Current IP Address:") + 20
+        Dim pEnd As Integer = ip.IndexOf("<", pStart)
+
+
+        ip = ip.Substring(pStart, pEnd - pStart)
+
+        Return ip
+    End Function
+
+End Module
